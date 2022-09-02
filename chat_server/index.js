3de583c3d3787b08637c85f9cc28af9063f7ae15
Rohit Shukla <rohit.shukla@qorbital.com>
2022-09-02 21:19:21 +0530

    port changed

--- chat_server/index.js
+++ chat_server/index.js
@@ -2,7 +2,7 @@ const express = require("express");
 var http = require("http");
 const cors = require("cors");
 const app = express();
-const port = process.env.PORT ;
+const port = process.env.PORT ||20;
 var server = http.createServer(app);
 var io = require("socket.io")(server,{
   cors:
